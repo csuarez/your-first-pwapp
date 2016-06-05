6780f6fe5dd91af78fee9e19f5959d7c29d30433
César Suárez <suarez.ortega.cesar@gmail.com>
2016-06-05 12:17:00 +0200

    Chapter 06 completed

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -195,4 +195,10 @@
     app.saveSelectedCities();
   }
 
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+
 })();
