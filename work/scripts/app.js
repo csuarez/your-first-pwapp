102062dc0c5a84dccd29bca646b286c4e0523b6f
César Suárez <suarez.ortega.cesar@gmail.com>
2016-06-05 12:02:38 +0200

    Chapter 05 completed

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -2,6 +2,33 @@
 (function() {
   'use strict';
 
+  var initialWeatherForecast = {
+   key: 'newyork',
+   label: 'New York, NY',
+   currently: {
+     time: 1453489481,
+     summary: 'Clear',
+     icon: 'partly-cloudy-day',
+     temperature: 52.74,
+     apparentTemperature: 74.34,
+     precipProbability: 0.20,
+     humidity: 0.77,
+     windBearing: 125,
+     windSpeed: 1.52
+   },
+   daily: {
+     data: [
+       {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
+       {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
+       {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
+       {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
+       {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
+       {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
+       {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
+     ]
+   }
+ };
+
   var app = {
     isLoading: true,
     visibleCards: {},
@@ -38,6 +65,7 @@
     var label = selected.textContent;
     app.getForecast(key, label);
     app.selectedCities.push({key: key, label: label});
+    app.saveSelectedCities();
     app.toggleAddDialog(false);
   });
 
@@ -147,5 +175,24 @@
       app.getForecast(key);
     });
   };
+
+  app.saveSelectedCities = function() {
+    var selectedCities = JSON.stringify(app.selectedCities);
+    localStorage.selectedCities = selectedCities;
+  };
+
+  app.selectedCities = localStorage.selectedCities;
+  if (app.selectedCities) {
+    app.selectedCities = JSON.parse(app.selectedCities);
+    app.selectedCities.forEach(function(city) {
+      app.getForecast(city.key, city.label);
+    });
+  } else {
+    app.updateForecastCard(initialWeatherForecast);
+    app.selectedCities = [
+      {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+    ];
+    app.saveSelectedCities();
+  }
 
 })();
